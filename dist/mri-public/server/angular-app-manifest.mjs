e49c2d86f0885bb6a30fcd9cc93e490119446bfe
Samuel Mandeng <zrotof@Air-de-Samuel.lan>
2025-12-12 18:58:37 +0100

    updates

--- dist/mri-public/server/angular-app-manifest.mjs
+++ dist/mri-public/server/angular-app-manifest.mjs
@@ -17,9 +17,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 5860, hash: '706edfee07ac180b2548aa6a1997a63ad33e86be59fd85d98ff7882806cb8f46', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 5594, hash: '4f0814cc14b594d11ff3c8b9cb90b88b22a1e45b121a96d293d42e10b95a92bc', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 49689, hash: '59cc399326e234f759609e3f31c4f8ab0f5028e3c7f8e19f2e8091e3ccad7ce4', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 5860, hash: 'bbb25482872df3c25331b678ecc6a5fc84ff532625b3fea1502f41a08f9ce5a6', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 5594, hash: 'c4acf92a218066ea631034cb24bd59c95b74d4da4df5baad38ab4a8a45372f78', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 49742, hash: 'ae31d547bf93f531b196310831c45332e95a8b89c1309fd05e38ae541bac8563', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-6D6JSZMT.css': {size: 15743, hash: 'ij5BS1UqpNw', text: () => import('./assets-chunks/styles-6D6JSZMT_css.mjs').then(m => m.default)}
   },
 };
